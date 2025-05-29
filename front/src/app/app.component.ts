282ffb97f9946093e81309eaae24d684a518a8b6
Eryan GAUVRIT <eryan.gauvrit@gmail.com>
2025-05-29 04:18:26 +0200

    Responsive side nav

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-import { SessionService } from './services/session.service';
 import { Observable } from 'rxjs';
+import { SessionService } from './services/session.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  
+    mobileQuery: MediaQueryList;
+    private _mobileQueryListener: () => void;
+  
     constructor(
       private router: Router,
-      private sessionService: SessionService) {
+      private sessionService: SessionService,
+      changeDetectorRef: ChangeDetectorRef,
+      media: MediaMatcher) {
+        this.mobileQuery = media.matchMedia('(max-width: 650px)');
+        this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+        this.mobileQuery.addListener(this._mobileQueryListener);
+    }
+
+    ngOnDestroy(): void {
+      this.mobileQuery.removeListener(this._mobileQueryListener);
     }
 
     public $isLogged(): Observable<boolean> {
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { JwtInterceptor } from './interceptors/jwt.interceptor';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomeComponent } from './pages/home/home.component';
 
 const materialModule = [
   MatButtonModule,
@@ -18,6 +20,8 @@ const materialModule = [
   MatIconModule,
   MatSnackBarModule,
   MatToolbarModule,
+  MatSidenavModule,
+  MatListModule
 ]
 
 @NgModule({
